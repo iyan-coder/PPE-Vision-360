3e256f07e4d8b4f037d188636a6dd02608ba0d21
iyancoder <iyanuoluwaadebayo04@gmail.com>
2025-08-20 07:12:59 -0700

    Add github/workflow/ci.yml

--- app/pages/1_PPE_Image_Compliance.py
+++ app/pages/1_PPE_Image_Compliance.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import sys
+import os
+# -----------------------------
+# Ensure src path is visible
+# -----------------------------
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from src.PPE_VISION_360.logger.logger import logger
+logger.info(f"Project root added to sys.path: {project_root}")
+
 from src.PPE_VISION_360.chat_engine.image_detection import ImageComplianceChecker
 from src.PPE_VISION_360.logger.logger import logger
 from src.PPE_VISION_360.exception.exception import PpeVision360Exception
