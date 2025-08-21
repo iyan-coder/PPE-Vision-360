7b2bf72ecb838e20b6cc625909b67ec5a346f92f
iyancoder <iyanuoluwaadebayo04@gmail.com>
2025-08-21 09:54:15 -0700

    update stramlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="PPE Vision 360", layout="wide")
+st.set_page_config(page_title="SafeSight AI Assistant", layout="wide")
 
 # ----------------- STYLES -----------------
 st.markdown("""
